65455154c2df883069ecf8e25ed0e6b5c8d8ab16
youliangdao <youliangdaojing5@gmail.com>
2022-08-11 15:33:12 +0900

    Feat: 席順をランダムにする

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,27 @@
 const seat = document.querySelector("#seat");
-let htmlStr = "";
-for (let i = 1; i < 19; i++) {
-  htmlStr += '<div class="seat__item">' + i + '</div>'
-}
 
+// for文を用いた実装法
+// let htmlStr = "";
+// for (let i = 1; i < 19; i++) {
+//   htmlStr += '<div class="seat__item">' + i + '</div>'
+// }
+// seat.innerHTML = htmlStr;
+
+// forEachを用いた実装法
+let htmlStr = "";
+const studentNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+for (let i = studentNumbers.length; i > 0; i--) {
+  const randomNum = Math.floor(Math.random() * i);
+  let tmp = studentNumbers[i - 1];
+  studentNumbers[i - 1] = studentNumbers[randomNum];
+  studentNumbers[randomNum] = tmp;
+}
+studentNumbers.forEach((val) => {
+  htmlStr += '<div class="seat__item">' + val + '</div>'
+})
+seat.innerHTML = htmlStr;
+
+// 一番原始的な実装方法
 // const htmlStr = `
 //       <div class="seat__item">1</div>
 //       <div class="seat__item">2</div>
@@ -24,5 +42,4 @@ for (let i = 1; i < 19; i++) {
 //       <div class="seat__item">17</div>
 //       <div class="seat__item">18</div>
 // `;
-
-seat.innerHTML = htmlStr;
+// seat.innerHTML = htmlStr;

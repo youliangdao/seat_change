72502a3eee45adae222caeec3762f8d82e2c97ce
youliangdao <youliangdaojing5@gmail.com>
2022-08-12 14:39:05 +0900

    Feat: ドラムロール音が止まったタイミングで席番号ブロックの繰り返しシャッフル表示を終了

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,41 @@
-const seat = document.querySelector("#seat");
+// forEachを用いた実装法
+const studentNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
+
+const shuffleArray = function () {
+  for (let i = studentNumbers.length; i > 0; i--) {
+    const randomNum = Math.floor(Math.random() * i);
+    let tmp = studentNumbers[i - 1];
+    studentNumbers[i - 1] = studentNumbers[randomNum];
+    studentNumbers[randomNum] = tmp;
+  }
+}
+
+const showSeatBoxes = function () {
+  let htmlStr = "";
+  studentNumbers.forEach((val) => {
+    htmlStr += '<div class="seat__item">' + val + '</div>'
+  })
+  document.querySelector("#seat").innerHTML = htmlStr;
+}
+
+const soundPlay = function (timer){
+  const audioElement = new Audio();
+  audioElement.src = 'assets/audio/drum.mp3';
+  audioElement.play();
+
+  audioElement.addEventListener("ended", () => {
+    clearInterval(timer)
+  })
+}
+
+document.getElementById("btn-start").addEventListener("click", () => {
+  const timer = setInterval(function(){
+    shuffleArray();
+    showSeatBoxes();
+  }, 50);
+
+  soundPlay(timer);
+})
 
 // for文を用いた実装法
 // let htmlStr = "";
@@ -6,20 +43,6 @@ const seat = document.querySelector("#seat");
 //   htmlStr += '<div class="seat__item">' + i + '</div>'
 // }
 // seat.innerHTML = htmlStr;
-
-// forEachを用いた実装法
-let htmlStr = "";
-const studentNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-for (let i = studentNumbers.length; i > 0; i--) {
-  const randomNum = Math.floor(Math.random() * i);
-  let tmp = studentNumbers[i - 1];
-  studentNumbers[i - 1] = studentNumbers[randomNum];
-  studentNumbers[randomNum] = tmp;
-}
-studentNumbers.forEach((val) => {
-  htmlStr += '<div class="seat__item">' + val + '</div>'
-})
-seat.innerHTML = htmlStr;
 
 // 一番原始的な実装方法
 // const htmlStr = `
